9524e7bbe09d128a6c1300cb5cea3df81c76c560
jonmalki <107192895+jonmalki@users.noreply.github.com>
2023-05-08 21:03:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const nameElement = document.querySelector('#name');
+  const name = 'Jonathan Malki';
+  nameElement.textContent = name;
+});
