40aa1bd9bba71728d439ff2d6180322a44d30633
jonmalki <107192895+jonmalki@users.noreply.github.com>
2023-05-08 21:12:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const nameElement = document.querySelector('#name');
-  const name = 'Jonathan Malki';
-  nameElement.textContent = name;
+// Get project list and sort dropdown
+const projectList = document.getElementById('project-list');
+const sortDropdown = document.getElementById('sort');
+
+// Function to sort projects by date
+function sortByDate() {
+  const projects = [...projectList.children];
+  projects.sort((a, b) => {
+    return new Date(b.dataset.date) - new Date(a.dataset.date);
+  });
+  projects.forEach(project => projectList.appendChild(project));
+}
+
+// Function to sort projects by name
+function sortByName() {
+  const projects = [...projectList.children];
+  projects.sort((a, b) => {
+    return a.querySelector('h2').textContent.localeCompare(b.querySelector('h2').textContent);
+  });
+  projects.forEach(project => projectList.appendChild(project));
+}
+
+// Event listener for sort dropdown change
+sortDropdown.addEventListener('change', () => {
+  if (sortDropdown.value === 'recent') {
+    sortByDate();
+  } else if (sortDropdown.value === 'name') {
+    sortByName();
+  }
 });
